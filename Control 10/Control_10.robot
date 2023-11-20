ba443e4a4699242a04b0d78e37ee6cddfe8fe121
Snizhana Chuiko <Snizhana.Chuiko@gmail.com>
2023-11-20 10:53:05 +0200

    Control task number 10 was added

---
+++ Control 10/Control_10.robot
@@ -0,0 +1,37 @@
+*** Settings ***
+Library           DatabaseLibrary
+
+*** Variables ***
+${DBname}         demo_database
+${DBuser}         root
+${DBpass}         Qwerty1@
+${DBhost}         localhost
+${DBport}         3306
+
+*** Test Cases ***
+Test 1
+    Connect to database    dbConfigFile=dbconfig.txt
+    Select age
+    Check if not Exists in database    Select * from demo1 where salary = 0;
+    [Teardown]    Disconnect from database
+
+Test 2
+    Connect To Database    pymysql    ${DBname}    ${DBuser}    ${DBpass}    ${DBhost}    ${DBport}
+    Execute sql string    Delete from demo1;
+    Row Count Is 0    Select * from demo1;
+    [Teardown]    Disconnect From Database
+
+Test 3
+    Connect to Database Using Custom Params    pymysql    database='${DBname}', user='${DBuser}', password='${DBpass}', host='${DBhost}'
+    Insert data
+    Row Count is Equal to X    Select * from demo1;    10
+    [Teardown]    Disconnect from database
+
+*** Keywords ***
+Insert data
+    ${result}    Execute sql script    ./data_for_demo1.sql
+    Log    ${result}
+
+Select age
+    ${result}    Query    Select name, surname from demo1 where age < 7;
+    log    people who yonger than 7 ${result}
---
+++ Control 10/insert into demo1.sql
@@ -0,0 +1,23 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2709
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;\f1\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red8\green69\blue140;\red0\green0\blue0;\red0\green0\blue0;
+\red149\green0\blue3;}
+{\*\expandedcolortbl;;\cssrgb\c0\c35294\c61961;\cssrgb\c0\c0\c0\c5882;\cssrgb\c0\c0\c0\c90196;
+\cssrgb\c65882\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs18 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 INSERT\cf4 \strokec4  \cf2 \strokec2 INTO\cf4 \strokec4  demo1 (\cf2 \strokec2 id\cf4 \strokec4 , \cf2 \strokec2 name\cf4 \strokec4 , surname, age, salary) \cf2 \strokec2 VALUES\cf4 \strokec4  (1, \cf5 \strokec5 'Jon'\cf4 \strokec4 , \cf5 \strokec5 'Do'\cf4 \strokec4 , 21, 10000);\
+\cf2 \strokec2 INSERT\cf4 \strokec4  \cf2 \strokec2 INTO\cf4 \strokec4  demo1 (\cf2 \strokec2 id\cf4 \strokec4 , \cf2 \strokec2 name\cf4 \strokec4 , surname, age, salary) \cf2 \strokec2 VALUES\cf4 \strokec4  (2, \cf5 \strokec5 'Ivan'\cf4 \strokec4 , \cf5 \strokec5 'Smith'\cf4 \strokec4 , 24, 12000);\
+\cf2 \strokec2 INSERT\cf4 \strokec4  \cf2 \strokec2 INTO\cf4 \strokec4  demo1 (\cf2 \strokec2 id\cf4 \strokec4 , \cf2 \strokec2 name\cf4 \strokec4 , surname, age, salary) \cf2 \strokec2 VALUES\cf4 \strokec4  (3, \cf5 \strokec5 'Marry'\cf4 \strokec4 , \cf5 \strokec5 'Popins'\cf4 \strokec4 , 17, 8000);\
+\cf2 \strokec2 INSERT\cf4 \strokec4  \cf2 \strokec2 INTO\cf4 \strokec4  demo1 (\cf2 \strokec2 id\cf4 \strokec4 , \cf2 \strokec2 name\cf4 \strokec4 , surname, age, salary) \cf2 \strokec2 VALUES\cf4 \strokec4  (4, \cf5 \strokec5 'Sergio'\cf4 \strokec4 , \cf5 \strokec5 'Trulle'\cf4 \strokec4 , 95, 0);\
+\cf2 \strokec2 INSERT\cf4 \strokec4  \cf2 \strokec2 INTO\cf4 \strokec4  demo1 (\cf2 \strokec2 id\cf4 \strokec4 , \cf2 \strokec2 name\cf4 \strokec4 , surname, age, salary) \cf2 \strokec2 VALUES\cf4 \strokec4  (5, \cf5 \strokec5 'Berta'\cf4 \strokec4 , \cf5 \strokec5 'Rassel'\cf4 \strokec4 , 34, 20000);\
+\cf2 \strokec2 INSERT\cf4 \strokec4  \cf2 \strokec2 INTO\cf4 \strokec4  demo1 (\cf2 \strokec2 id\cf4 \strokec4 , \cf2 \strokec2 name\cf4 \strokec4 , surname, age, salary) \cf2 \strokec2 VALUES\cf4 \strokec4  (6, \cf5 \strokec5 'Trune'\cf4 \strokec4 , \cf5 \strokec5 'Urroro'\cf4 \strokec4 , 45, 30000);\
+\cf2 \strokec2 INSERT\cf4 \strokec4  \cf2 \strokec2 INTO\cf4 \strokec4  demo1 (\cf2 \strokec2 id\cf4 \strokec4 , \cf2 \strokec2 name\cf4 \strokec4 , surname, age, salary) \cf2 \strokec2 VALUES\cf4 \strokec4  (7, \cf5 \strokec5 'Agneta'\cf4 \strokec4 , \cf5 \strokec5 'Bartovna'\cf4 \strokec4 , 60, 15000);\
+\cf2 \strokec2 INSERT\cf4 \strokec4  \cf2 \strokec2 INTO\cf4 \strokec4  demo1 (\cf2 \strokec2 id\cf4 \strokec4 , \cf2 \strokec2 name\cf4 \strokec4 , surname, age, salary) \cf2 \strokec2 VALUES\cf4 \strokec4  (8, \cf5 \strokec5 'Violetta'\cf4 \strokec4 , \cf5 \strokec5 'White'\cf4 \strokec4 , 7, 0);\
+\cf2 \strokec2 INSERT\cf4 \strokec4  \cf2 \strokec2 INTO\cf4 \strokec4  demo1 (\cf2 \strokec2 id\cf4 \strokec4 , \cf2 \strokec2 name\cf4 \strokec4 , surname, age, salary) \cf2 \strokec2 VALUES\cf4 \strokec4  (9, \cf5 \strokec5 'Lion'\cf4 \strokec4 , \cf5 \strokec5 'Goodwin'\cf4 \strokec4 , 2, 99999);\
+\cf2 \strokec2 INSERT\cf4 \strokec4  \cf2 \strokec2 INTO\cf4 \strokec4  demo1 (\cf2 \strokec2 id\cf4 \strokec4 , \cf2 \strokec2 name\cf4 \strokec4 , surname, age, salary) \cf2 \strokec2 VALUES\cf4 \strokec4  (10, \cf5 \strokec5 'Karetto'\cf4 \strokec4 , \cf5 \strokec5 'Jovanny'\cf4 \strokec4 , 50, 50000);
+\f1 \
+}
